94f75a864419e30022c91143a005bbc8dbc451d6
efti-dot <eftih31@gmail.com>
2025-11-07 04:46:16 +0600

    add pdf uploader

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
 def AI_insurance_assistance():
-    st.title("Hello! I'm your AI Insurance Assistance Bot.")
+    st.title("AI Insurance Assistance")
+
+    uploaded_file = st.file_uploader("Upload your PDF", type=["pdf"])
+    if uploaded_file and "pdf_chunks" not in st.session_state:
+        st.success("PDF processed and ready!")
 
 
-def main():
+if __name__ == "__main__":
     AI_insurance_assistance()
